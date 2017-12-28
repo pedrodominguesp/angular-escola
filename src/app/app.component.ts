b7531ee3ab2167dcf4fddc751ff3c91f5fce2cd3
Domingues <pedro.h.domingues@accenture.com>
2017-12-28 17:11:29 -0200

    firts commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Disciplina } from './disciplina.model';
+import {RouterModule, Routes} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  disciplinas =[
+    new Disciplina('Portugues', 'descricao linguagem pt-br'),
+    new Disciplina('Matematica', 'descricao linguagem pt-br'),
+    new Disciplina('Ingles', 'descricao linguagem pt-br'),
+    new Disciplina('Ciencias', 'descricao linguagem pt-br'),
+
+  ];
+
+  selecionado = null;
+  nome = null;
+  descricao = null;
+  editando = null;
+
+  selecionar(disciplina){
+    this.selecionado = disciplina;
+  }
+  salvar(){
+    const d = new Disciplina(this.nome, this.descricao);
+    this.disciplinas.push(d);
+    this.nome = null;
+    this.descricao = null;
+  }
+
+  excluir(disciplina){
+    if (confirm('Deseja excluir a disciplina: ' + disciplina.nome + '?')) {
+      const i = this.disciplinas.indexOf(disciplina);
+      this.disciplinas.splice(i, 1);
+    }
+  }
+
+  editar(disciplina){
+    this.nome = disciplina.nome;
+    this.descricao = disciplina.descricao;
+    this.editando = disciplina;
+  }
+
+  cancelar(){
+    this.nome = null;
+    this.descricao = null;
+    this.editando = null;
+  }
+}
---
+++ src/app/disciplina.model.ts
@@ -0,0 +1,10 @@
+export class Disciplina{
+    nome: String;
+    descricao: String;
+    
+    constructor(nome: string, descricao?: string) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+}
+
